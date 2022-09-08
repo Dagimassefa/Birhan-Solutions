4f5c69c15be5f972fa0625502b048668f8e8bfec
Dagimassefa <kuku.assefa18@gmail.com>
2022-09-08 18:14:31 +0300

    updating additional skills and making get in touch a popup screen

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,49 @@ function sendMails(params) {
         message: document.getElementById("message").value,
 
     };
-    emailjs.send('service_djxgh0d', 'template_wjb707a', tempParams).then(function (res) {
-        console.log("sucess", res.status);
+    if (document.getElementById("fname").value != "" && document.getElementById("lname").value != "" && document.getElementById("email").value != "" && document.getElementById("message").value != "") {
+        var regfname = /^[a-zA-Z]+$/;
+        var fname = document.getElementById('fname').value;
+        if (!regfname.test(fname)) {
+            alert('Invalid first name given.');
+        } else {
+            var lname = document.getElementById('lname').value;
+            var reglname = /^[a-zA-Z]+$/;
+            if (!reglname.test(lname)) {
+                alert('Invalid last name given.');
+            } else {
+                var email = document.getElementById('email');
+                var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+                if (!filter.test(email.value)) {
+                    alert('Please provide a valid email address');
+                }
+                else {
+
+                    emailjs.send('service_djxgh0d', 'template_wjb707a', tempParams).then(function (res) {
+                        console.log("sucess", res.status);
 
 
-    })
-    document.getElementById("fname").value = "";
+                    })
+                    alert("all things look good!")
+                    document.getElementById("fname").value = "";
+                    document.getElementById("lname").value = "";
+                    document.getElementById("email").value = "";
+                    document.getElementById("phone").value = "";
+                    document.getElementById("message").value = "";
+                }
+            }
+
+
+        }
+    }
+    else {
+        alert("all inputs except phone number is mandatory!!!")
+        document.getElementById("fname").value = "";
+        document.getElementById("lname").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("phone").value = "";
+        document.getElementById("message").value = "";
+    }
+
+
 }
--- js/subscribe.js
+++ js/subscribe.js
@@ -3,10 +3,24 @@ function sendMail(params) {
         message: document.getElementById("sus_email").value,
 
     };
-    emailjs.send('service_djxgh0d', 'template_76co85s', tempParams).then(function (res) {
-        console.log("sucess", res.status);
+    if (document.getElementById("sus_email").value != "") {
+        var email = document.getElementById('sus_email');
+        var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if (!filter.test(email.value)) {
+            alert('Please provide a valid email address');
+        }
+        else {
+            emailjs.send('service_djxgh0d', 'template_76co85s', tempParams).then(function (res) {
+                console.log("sucess", res.status);
 
-    })
+            })
+            alert("you have successfully subscribed!")
+        }
+    }
+    else {
+        alert("please enter an email address");
+    }
+
     // document.getElementById("sus_email").value = "";
-    document.getElementById("sus_email").value = "";
+    // document.getElementById("sus_email").value = "";
 }
